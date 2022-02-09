8f4c386ab334a3acc56866b98621aa4fe567c2be
CesarAugusto88 <cesarcosta.augustos@gmail.com>
2022-02-09 09:48:38 -0300

    Video 3 finalizado. Pesquisa de Livros.

---
+++ testeMysql/Form1.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.CbCampos = New System.Windows.Forms.ComboBox()
+        Me.TxtProcurar = New System.Windows.Forms.TextBox()
+        Me.BtnBuscar = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(79, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Click this"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(86, 50)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 13)
+        Me.Label1.TabIndex = 1
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 149)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(847, 282)
+        Me.DataGridView1.TabIndex = 2
+        '
+        'CbCampos
+        '
+        Me.CbCampos.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.CbCampos.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CbCampos.FormattingEnabled = True
+        Me.CbCampos.Location = New System.Drawing.Point(12, 116)
+        Me.CbCampos.Name = "CbCampos"
+        Me.CbCampos.Size = New System.Drawing.Size(175, 24)
+        Me.CbCampos.TabIndex = 3
+        '
+        'TxtProcurar
+        '
+        Me.TxtProcurar.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtProcurar.Location = New System.Drawing.Point(193, 116)
+        Me.TxtProcurar.MaxLength = 90
+        Me.TxtProcurar.Name = "TxtProcurar"
+        Me.TxtProcurar.Size = New System.Drawing.Size(433, 23)
+        Me.TxtProcurar.TabIndex = 4
+        '
+        'BtnBuscar
+        '
+        Me.BtnBuscar.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnBuscar.Location = New System.Drawing.Point(632, 112)
+        Me.BtnBuscar.Name = "BtnBuscar"
+        Me.BtnBuscar.Size = New System.Drawing.Size(75, 31)
+        Me.BtnBuscar.TabIndex = 5
+        Me.BtnBuscar.Text = "Buscar"
+        Me.BtnBuscar.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(307, 92)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(121, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "PESQUISA DE LIVROS"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(871, 472)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.BtnBuscar)
+        Me.Controls.Add(Me.TxtProcurar)
+        Me.Controls.Add(Me.CbCampos)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Test Connection MySQL"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents CbCampos As System.Windows.Forms.ComboBox
+    Friend WithEvents TxtProcurar As System.Windows.Forms.TextBox
+    Friend WithEvents BtnBuscar As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ testeMysql/Form1.vb
@@ -0,0 +1,115 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Using conexao = New MySqlConnection("server=localhost;port=3306;database=gstec;uid=root;password='1234' ")
+                conexao.Open()
+                Label1.Text = "HELLO WORLD! CONEXÃO BEM SUCEDIDA BANCO GSTEC"         
+            End Using
+        Catch ex As Exception
+            MsgBox("Falha na conexão: " + ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PopularCbCampos()
+        DataGridView1.DataSource = GetLivros("")
+        If DataGridView1.RowCount > 0 Then
+            ConfigurarGrade()
+        End If
+    End Sub
+
+    Private Sub PopularCbCampos()
+        With CbCampos
+            .Items.Clear()
+            .Items.Add("Título")
+            .Items.Add("Autor")
+            .Items.Add("Preço até")
+        End With
+    End Sub
+
+    Private Function GetLivros(filtro As String) As DataTable
+        Dim sql = "Select id, titulo, autores, isbn, unitario, ativo from livros WHERE ativo = 'S'"
+        Dim dt As New DataTable
+
+        If filtro <> "" Then
+            If CbCampos.Text = "Título" Then
+                sql = sql & "AND titulo LIKE '%" & filtro & "%'"
+            ElseIf CbCampos.Text = "Autor" Then
+                sql = sql & "AND autores LIKE '%" & filtro & "%'"
+            ElseIf CbCampos.Text = "Preço até" Then
+                filtro = filtro.Replace(",", ".")
+                If IsNumeric(filtro) Then
+                    sql = sql & "AND unitario <= " & filtro
+                Else
+                    MsgBox("A procura precisa ser um número!")
+                End If
+            End If
+        End If
+
+        Try
+            Using conexao = New MySqlConnection("server=localhost;port=3306;database=livros;uid=root;password='1234' ")
+                conexao.Open()
+                Using da = New MySqlDataAdapter(sql, conexao)
+                    da.Fill(dt)
+                End Using
+            End Using
+
+        Catch ex As Exception
+            MsgBox("Falha na conexão: " & ex.Message)
+            dt = Nothing
+
+        End Try
+
+        Return dt
+
+    End Function
+
+    Private Sub ConfigurarGrade()
+
+        With DataGridView1
+
+            DataGridView1.DefaultCellStyle.Font = New Font("Arial", 9)
+            DataGridView1.RowHeadersWidth = 25
+
+            DataGridView1.Columns("id").HeaderText = "ID"
+            DataGridView1.Columns("id").Visible = False
+
+            DataGridView1.Columns("titulo").HeaderText = "Título"
+            DataGridView1.Columns("titulo").Width = 350
+
+            DataGridView1.Columns("autores").HeaderText = "Autores"
+            DataGridView1.Columns("autores").Width = 230
+
+            DataGridView1.Columns("isbn").HeaderText = "ISBN"
+            DataGridView1.Columns("isbn").Width = 120
+            DataGridView1.Columns("isbn").HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter
+            DataGridView1.Columns("isbn").DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+
+            DataGridView1.Columns("unitario").HeaderText = "Preço"
+            DataGridView1.Columns("unitario").Width = 60
+            DataGridView1.Columns("unitario").HeaderCell.Style.Alignment =
+                DataGridViewContentAlignment.MiddleRight
+            DataGridView1.Columns("unitario").DefaultCellStyle.Alignment =
+                DataGridViewContentAlignment.MiddleRight
+
+            DataGridView1.Columns("ativo").HeaderText = "Ativo"
+            DataGridView1.Columns("ativo").Width = 40
+            DataGridView1.Columns("ativo").HeaderCell.Style.Alignment =
+                DataGridViewContentAlignment.MiddleCenter
+            DataGridView1.Columns("ativo").DefaultCellStyle.Alignment =
+                DataGridViewContentAlignment.MiddleCenter
+
+        End With
+
+    End Sub
+
+    Private Sub BtnBuscar_Click(sender As Object, e As EventArgs) Handles BtnBuscar.Click
+        DataGridView1.DataSource = GetLivros(TxtProcurar.Text)
+        If DataGridView1.RowCount > 0 Then
+            ConfigurarGrade()
+        End If
+    End Sub
+End Class
